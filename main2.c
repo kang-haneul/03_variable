6416e6e36ea6a9bccea8430c46fafadf8425ed7d
kang-haneul <55428030+kang-haneul@users.noreply.github.com>
2019-09-19 15:28:57 +0900

    shilsup2

---
+++ main2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+
+	int x;
+	short y;
+	
+	printf("byte of int : %d\n", sizeof(x));
+	
+	printf("byte of short : %d\n", sizeof(y));
+
+
+
+	return 0;
+}
